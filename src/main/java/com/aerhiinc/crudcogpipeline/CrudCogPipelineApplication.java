4f31d4537cef6f4148a8c2637d56aac17d5d37a5
Kingsley Erhi <2002041@cognizant.com>
2020-04-19 12:56:48 -0400

    Initial 2

---
+++ src/main/java/com/aerhiinc/crudcogpipeline/CrudCogPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.aerhiinc.crudcogpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCogPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCogPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aerhiinc/crudcogpipeline/CrudCogPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aerhiinc.crudcogpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCogPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
